ed55cda5c4e4486f7b0ab3769fd58b8b9c0085c7
Ken Sharp <KenSharp@users.noreply.github.com>
2018-11-16 18:17:36 +0000

    Update Ubuntu_pre_install.sh
    * Bash isn't needed or desirable
    * This only works on Xenial
    * Remove duplication

--- Wallet Direct Downloads/Version 2.1 - Mandatory Update/Ubuntu_pre_install.sh
+++ Wallet Direct Downloads/Version 2.1 - Mandatory Update/Ubuntu_pre_install.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
+# Ubuntu Xenial pre-installation script.
+# This will fail on Bionic unless you enable
+# the Xenial repositories. Create a file in
+# /etc/apt/sources.list.d/xenial.list:
+# deb http://archive.ubuntu.com/ubuntu xenial-updates main universe
+# deb http://security.ubuntu.com/ubuntu xenial-security main universe
+
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install libboost-system1.58.0
-sudo apt-get install libboost-filesystem1.58.0
-sudo apt-get install libboost-program-options1.58.0
-sudo apt-get install libboost-thread1.58.0
-sudo apt-get install libboost-chrono1.58.0
-sudo apt-get install libminiupnpc10
-sudo apt-get install libzmq5
-sudo apt-get install libevent-2.0-5
-sudo apt-get install libevent-pthreads-2.0-5
-sudo apt-get install libcurl4-openssl-dev
+sudo apt-get dist-upgrade
+sudo apt-get install libboost-system1.58.0 libboost-filesystem1.58.0 libboost-program-options1.58.0 libboost-thread1.58.0 libboost-chrono1.58.0 libminiupnpc10 libzmq5 libevent-2.0-5 libevent-pthreads-2.0-5 libcurl4-openssl-dev
